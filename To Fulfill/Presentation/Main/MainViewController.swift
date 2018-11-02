729f66d96d8f4df65afd14d29e4e878cfd495a4a
Nazarii Melnyk <nazarij.melnyk@gmail.com>
2018-11-02 21:34:22 +0200

    Added initial controllers and storyboards

--- To Fulfill/Presentation/Main/MainViewController.swift
+++ To Fulfill/Presentation/Main/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  To Fulfill
 //
 //  Created by Nazarii Melnyk on 11/2/18.
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
