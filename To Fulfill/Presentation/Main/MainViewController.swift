7bf69c475fca4ceb7f7343db0056d04759fbc1d6
nazarii.melnyk <nazarii.melnyk@globallogic.com>
2019-05-02 13:03:50 +0300

    Add animation for main menu views

--- To Fulfill/Presentation/Main/MainViewController.swift
+++ To Fulfill/Presentation/Main/MainViewController.swift
@@ -12,6 +12,8 @@ class MainViewController: ViewController {
     
     private let localizedStrings = Localizator.shared.localizedStrings?.main
     
+    @IBOutlet private weak var logoItemsStackView: UIStackView!
+    @IBOutlet private weak var curvedLineView: CurvedLineView!
     @IBOutlet private weak var conversationButton: UIButton!
     @IBOutlet private weak var storyButton: UIButton!
     @IBOutlet private weak var specialThanksButton: UIButton!
@@ -22,6 +24,38 @@ class MainViewController: ViewController {
         conversationButton.setTitle(localizedStrings?.conversationButtonsLabel, for: .normal)
         storyButton.setTitle(localizedStrings?.storyButtonsLabel, for: .normal)
         specialThanksButton.setTitle(localizedStrings?.specialThanksButtonsLabel, for: .normal)
+        showViews()
+    }
+    
+    private func showViews() {
+        logoItemsStackView.alpha = 0
+        curvedLineView.alpha = 0
+        conversationButton.alpha = 0
+        storyButton.alpha = 0
+        specialThanksButton.alpha = 0
+        
+        let buttonsAnimation: (Bool) -> Void = { [weak self] _ in
+            UIView.animate(withDuration: 0.35, animations: {
+                self?.conversationButton.alpha = 1
+            }) { _ in
+                UIView.animate(withDuration: 0.35, animations: {
+                    self?.storyButton.alpha = 1
+                }) { _ in
+                    UIView.animate(withDuration: 0.35, animations: {
+                        self?.specialThanksButton.alpha = 1
+                    })
+                }
+            }
+        }
+        let curvedLineAnimation: (Bool) -> Void = { [weak self] _ in
+            UIView.animate(withDuration: 0.5, animations: {
+                self?.curvedLineView.alpha = 1
+            }, completion: buttonsAnimation)
+        }
+        
+        UIView.animate(withDuration: 0.9, animations: {
+            self.logoItemsStackView.alpha = 1
+        }, completion: curvedLineAnimation)
     }
     
     @IBAction private func conversationButtonClick(_ sender: UIButton) {
