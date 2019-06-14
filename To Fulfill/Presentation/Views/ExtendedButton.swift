15f9554bc0edfd5a948dca0d6c9b7ec145a26c4f
nazarii.melnyk <nazarii.melnyk@globallogic.com>
2019-06-14 19:54:09 +0300

    Make "Back" button round

--- To Fulfill/Presentation/Views/ExtendedButton.swift
+++ To Fulfill/Presentation/Views/ExtendedButton.swift
@@ -20,8 +20,28 @@ class ExtendedButton: UIButton {
     @IBInspectable
     var borderWidth: CGFloat = 0 {
         didSet {
-            layer.borderWidth = borderWidth
+            layer.borderWidth = isCircle ? 0 : borderWidth
         }
+    }
+    @IBInspectable
+    var isCircle: Bool = false {
+        didSet {
+            layer.borderWidth = isCircle ? 0 : borderWidth
+        }
+    }
+    
+    override func draw(_ rect: CGRect) {
+        guard isCircle else {
+            super.draw(rect)
+            return
+        }
+        
+        let radius = min(bounds.width, bounds.height) / 2 * 0.95
+        let center = CGPoint(x: bounds.midX, y: bounds.midY)
+        let path = UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: 2 * .pi, clockwise: false)
+        path.lineWidth = borderWidth
+        borderColor.set()
+        path.stroke()
     }
     
 }
